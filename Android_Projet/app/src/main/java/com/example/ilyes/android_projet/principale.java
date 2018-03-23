eb48d9df0ab8ba2c652637343798c5aa87daae27
marouj <30570692+marouj@users.noreply.github.com>
2018-03-23 16:00:00 +0100

    Ajout nouveau projeyt android
    1-Supression ancien projet android
    2-Ajout nouveau projet Android

---
+++ Android_Projet/app/src/main/java/com/example/ilyes/android_projet/principale.java
@@ -0,0 +1,13 @@
+package com.example.ilyes.android_projet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class principale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principale);
+    }
+}
